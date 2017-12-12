8c713c14d74fc8c4a5ebc49ca93ee0621fe3543c
Dhanraj N <dhanrajk@Dk.local>
2017-12-12 16:46:03 +0530

    demo branch

--- GIT Tutorial/ViewController.swift
+++ GIT Tutorial/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        //New changes
+        //New changes jjj
+        //demo branch
     }
 
     override func didReceiveMemoryWarning() {
